5d019b74fca05f2bd78a4ec7b54b420308d7bd4a
Lê Hồng Minh <637942@sv.vnua.edu.vn>
2022-03-27 11:34:55 +0700

    add dash board

--- app/Http/Livewire/Admin/Home/Dashboard.php
+++ app/Http/Livewire/Admin/Home/Dashboard.php
@@ -9,9 +9,7 @@ class Dashboard extends Component
 {
     public function render()
     {
-        $posts = Post::query()->with(['categories', 'users'])->limit(3)->get();
         return view('livewire.admin.home.dashboard', [
-            'posts' => $posts
         ])
             ->extends('admin.layouts.master')->section('content');
     }
